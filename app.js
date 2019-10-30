ec397a5a980f795a7888d1aa5dc202c7bfd30f49
Peter Nordman <petermnordman@gmail.com>
2019-10-30 12:43:51 -0400

    add views, initial styling, event handling, questions library

--- app.js
+++ app.js
@@ -9,36 +9,30 @@ const STORE = {
 }
 
 // Questions Library
-const questions = [
-    {
-        question: '',
-        answers: [
-            {
-                text: '',
-                correct: false;
-            }
-            {
-                text: '',
-                correct: false;
-            }
-            {
-                text: '',
-                correct: false;
-            }
-        ]
-    }
-]
+    // stored in questions.js and linked in html
 
 /* -------------------------------------------------------------------------- */
 /*                                    VIEW                                   */
 /* -------------------------------------------------------------------------- */
 
 function renderQuestions () {
+    // Clear the main element
+    $('main').html('');
     // If currentQuestion is larger than questions.length, render final screen
-    //
-    // Else, assign questions[currentIndex] to a variable
-    // Use question object to set question text 
-    // Using a loop, append answers to <ol>, assigning variables in an HTML string
+    if (STORE.currentQuestion >= questions.length) {
+        $('main').html('')
+    } else {
+        // Else, assign questions[currentIndex] to a variable
+        let currentQuestion = questions[STORE.currentQuestion];
+        let questionCounter = `<span>${STORE.currentQuestion + 1}/${questions.length}</span>`
+        // Use question object to set question text 
+        $('main').append(`${questionCounter}<p class="question">${currentQuestion.question}</p><ol></ol>`)
+        // Using a loop, append answers to <ol>, assigning variables in an HTML string
+        $('ol').append(`
+            <li class="check">${currentQuestion.answers[0].text}</li>
+            <li class="check">${currentQuestion.answers[1].text}</li>
+            <li class="check">${currentQuestion.answers[2].text}</li>`)
+    }
 }
 
 /* -------------------------------------------------------------------------- */
@@ -48,20 +42,31 @@ function renderQuestions () {
 function checkAnswer () {
     // If correct, add 1 to score
     // Append span with feedback and 'next' button
+    getNextQuestion();
 }
 
 function getNextQuestion () {
     // currentQuestion++
+    STORE.currentQuestion++;
     // call renderQuestions
+    renderQuestions();
+}
+
+function startQuiz () {
+    // Set score to 0
+    STORE.score = 0;
+    // Set currentQuestion to 0
+    STORE.currentQuestion = 0;
+
+    renderQuestions();
 }
 
 function init () {
-    // Clear current 'main'
-    // Set score to 0
-    // Set currentQuestion to 0
-    // Add event listeners on 'main' for each button class
-        // .next, .check, .init
-    // Call render
+    // Add event handlers on 'main' for each button class
+    $('main').on('click', '.start', startQuiz);
+    $('main').on('click', '.check', checkAnswer);
+    $('main').on('click', '.next', getNextQuestion);
+        // .next, .check, .start
 }
 
 init();
---
+++ questions.js
@@ -0,0 +1,177 @@
+
+const questions = [
+    {
+        question: 'What is the purpose of the doctype command?',
+        answers: [
+            {
+                text: 'It is not required.',
+                correct: false
+            },
+            {
+                text: 'To link to outside files.',
+                correct: false
+            },
+            {
+                text: 'To tell the browser how to interpret the document.',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'What is a <head> element?',
+        answers: [
+            {
+                text: 'Where you should place your `h1` element.',
+                correct: false
+            },
+            {
+                text: 'A section of the code for providing metadata about the document.',
+                correct: true
+            },
+            {
+                text: 'A section of the code for providing styling and layout instructions.',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What is spaghetti code?',
+        answers: [
+            {
+                text: 'Code that does not follow good accessibility standards or responsive design patterns.',
+                correct: false
+            },
+            {
+                text: 'Code that is difficult to read and maintain, and that is not scaleable.',
+                correct: true
+            },
+            {
+                text: 'A system of ancient Florentine sumptuary laws.',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Where should you place the lang="en" attribute?',
+        answers: [
+            {
+                text: 'On the html tag.',
+                correct: true
+            },
+            {
+                text: 'On the DOCTYPE.',
+                correct: false
+            },
+            {
+                text: 'On the body tag.',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Explain the `static` CSS property.',
+        answers: [
+            {
+                text: 'Default; follows normal page flow.',
+                correct: true
+            },
+            {
+                text: 'Removed from normal document flow and ignored by other elements.',
+                correct: false
+            },
+            {
+                text: 'Normal flow; enables use of left/right/top/bottom/z-index.',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Explain the `absolute` CSS property.',
+        answers: [
+            {
+                text: 'Normal flow; enables use of left/right/top/bottom/z-index.',
+                correct: false
+            },
+            {
+                text: 'Removed from flow, unaffected by scrolling and relative to the document.',
+                correct: false
+            },
+            {
+                text: 'Removed from normal document flow and ignored by other elements.',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'Explain the `relative` CSS property.',
+        answers: [
+            {
+                text: 'Normal flow; enables use of left/right/top/bottom/z-index.',
+                correct: true
+            },
+            {
+                text: 'Default; follows normal page flow.',
+                correct: false
+            },
+            {
+                text: 'Removed from flow, unaffected by scrolling and relative to the document.',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Explain the `fixed` CSS property.',
+        answers: [
+            {
+                text: 'Removed from flow, unaffected by scrolling and relative to the document.',
+                correct: true
+            },
+            {
+                text: 'Default; follows normal page flow.',
+                correct: false
+            },
+            {
+                text: 'Removed from normal document flow and ignored by other elements.',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What does `pass by reference` mean?',
+        answers: [
+            {
+                text: 'JavaScript assigns primitive types as references to their place in memory.',
+                correct: false
+            },
+            {
+                text: 'JavaScript assigns variables a reference to objects by default, rather than creating a new object.',
+                correct: true
+            },
+            {
+                text: 'Functions are unable to mutate any variable that is passed to them as an argument.',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What syntax is used to iterate through each property in an object?',
+        answers: [
+            {
+                text: 'for (let i = 1; i < myObj.length; i++) {myObj[i]}',
+                correct: false
+            },
+            {
+                text: 'myObj.forEach()',
+                correct: false
+            },
+            {
+                text: 'for (each in myObj) {}',
+                correct: true
+            }
+        ]
+    },
+    
+]
+
+
+
