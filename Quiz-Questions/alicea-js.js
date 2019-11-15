0ec36ca113a31e37517e2d4fb525a8888514ca89
Peter Nordman <petermnordman@gmail.com>
2019-11-15 09:12:30 -0500

    new questions

---
+++ Quiz-Questions/alicea-js.js
@@ -0,0 +1,19 @@
+const questions = [
+    {
+        question: '',
+        answers: [
+            {
+                text: '',
+                correct: false
+            },
+            {
+                text: '',
+                correct: false
+            },
+            {
+                text: '',
+                correct: true
+            }
+        ]
+    },
+]
---
+++ Quiz-Questions/alicea-node.js
@@ -0,0 +1,87 @@
+const aliceaNodeQuestions = [
+    {
+        question: 'What language is NodeJS written in?',
+        answers: [
+            {
+                text: 'JavaScript',
+                correct: false
+            },
+            {
+                text: 'Assembly',
+                correct: false
+            },
+            {
+                text: 'C++',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            {
+                text: '',
+                correct: false
+            },
+            {
+                text: '',
+                correct: false
+            },
+            {
+                text: '',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            {
+                text: '',
+                correct: false
+            },
+            {
+                text: '',
+                correct: false
+            },
+            {
+                text: '',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            {
+                text: '',
+                correct: false
+            },
+            {
+                text: '',
+                correct: false
+            },
+            {
+                text: '',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            {
+                text: '',
+                correct: false
+            },
+            {
+                text: '',
+                correct: false
+            },
+            {
+                text: '',
+                correct: true
+            }
+        ]
+    },
+]
