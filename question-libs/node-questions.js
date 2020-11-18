e97ac40427c4c3d9bab47b74b0db1e673fd963e6
Peter Nordman <peter.m.nordman@gmail.com>
2020-11-18 10:25:47 -0500

    Add a few more Node.js questions

--- question-libs/node-questions.js
+++ question-libs/node-questions.js
@@ -1,6 +1,18 @@
 const nodeQuestions = [
   {
-    question: "What language is NodeJS written in?",
+    question: "What language is Node.js written in?",
     answer: "C++",
+  },
+  {
+    question: "What is the Node.js event loop?",
+    answer: "It listens for system events and triggers a callback if any are registered",
+  },
+  {
+    question: "How might you read a file in Node.js?",
+    answer: "fs.readFile(path) or fs.readFileSync(path)",
+  },
+  {
+    question: "How do you start a Node.js server without Express?",
+    answer: "require('http').createServer((req, res) => {}).listen(3000, () => console.log('listening on port 3000'))",
   },
 ];
